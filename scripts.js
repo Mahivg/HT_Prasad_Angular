1022be56bed9a469269fb68de994d6dd53e57f95
MageshVg5 <vgmagesh5@gmail.com>
2021-01-07 11:05:59 +0530

    Till javascript

---
+++ scripts.js
@@ -0,0 +1,65 @@
+var headings = document.getElementById("headings");
+// console.log(myElement);
+// console.dir(myElement);
+// myElement.textContent = "Test Changed";
+var paragraph = document.getElementById("paragraph");
+paragraph.appendChild(headings);
+
+function printConsoleOnClick() {
+    console.log("clicked on paragraph...");
+}
+
+var printInConsoleOnClick = function() {
+    console.log("clicked on paragraph...");
+}
+
+var addition = function(num1, num2) {
+    return num1 + num2;
+}
+
+var d = d => d* 2;
+
+array.map(function(d){
+    return d * 2;
+});
+
+array.map(d => d *2);
+array.map(d);
+
+
+var additionL = (num1, num2) =>  num1 + num2 ;
+var additionL = (num1, num2) => { console.log(d); return num1 + num2 };
+// Event Listeners
+paragraph.addEventListener("mouseover", printInConsoleOnClick);
+
+
+function extractFormValues() {
+    var inputTxt = document.getElementById("inputTxt");
+
+    var arrayOfH2 = document.getElementsByTagName('h2');
+
+    document.querySelectorAll('.box');
+
+
+    var name = inputTxt.value;
+
+    var inputNum = document.getElementById("inputNum");
+    var age = inputNum.value;
+
+    inputNum.value = 28;
+
+    var inputEmail = document.getElementById("inputEmail");
+    var email = inputEmail.value;
+
+    var inputPass = document.getElementById("inputPass");
+    var password = inputPass.value;
+
+    var user = {
+        'name' : name,
+        'age' : age,
+        'email' : email,
+        'password': password
+    };
+    alert("Value Submitted.."  + JSON.stringify(user));
+}
+
